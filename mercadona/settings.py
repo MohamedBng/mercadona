c52419e25c18bcda509f00993065840ca444f452
Mohamed Bengrich <mohamed@etamin.studio>
2023-11-08 22:31:32 +0100

    wip

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m((uy-@orl8p2eh$gpz$)-vh8wn@k@5d$7n()%38gtr!@h#=oz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mercadonastudi-c95dd55a3900.herokuapp.com']
 
