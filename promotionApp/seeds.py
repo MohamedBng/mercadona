a2597d4025dc702426720abcd9713fe71e3a1485
Mohamed Bengrich <mohamed@etamin.studio>
2023-11-08 22:20:52 +0100

    wip

--- promotionApp/seeds.py
+++ promotionApp/seeds.py
@@ -1,38 +1,57 @@
 import os
 import django
+from django.core.management import call_command
 from promotionApp.models import Product, Category, Promotion
 from djmoney.money import Money
 from django.utils import timezone
 from datetime import timedelta
-from django.db import transaction
 
+# Initialisez Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mercadona.settings")
 django.setup()
 
-@transaction.atomic
-def run():
-    categories_data = ['Aliments frais', 'Aliments emballés', 'Boissons', 'Produits de beauté et d\'hygiène', 'Nettoyage de la maison']
-    categories = [Category(label=cat) for cat in categories_data]
-    Category.objects.bulk_create(categories)
+class Command(BaseCommand):
+    help = 'Seeds the database with initial data'
 
-    products_data = {
-        'Aliments frais': ['Poulet', 'Poisson', 'Jambon', 'Tomates', 'Laitue'],
-        'Aliments emballés': ['Pâtes', 'Riz', 'Céréales', 'Conserves de thon', 'Biscuits'],
-        'Boissons': ['Vin', 'Bière', 'Jus d\'orange', 'Eau', 'Limonade'],
-        'Produits de beauté et d\'hygiène': ['Shampooing', 'Gel douche', 'Crème hydratante', 'Dentifrice', 'Déodorant'],
-        'Nettoyage de la maison': ['Détergent', 'Nettoyant sol', 'Liquide vaisselle', 'Spray nettoyant', 'Eponge']
-    }
+    def handle(self, *args, **kwargs):
+        # Catégories
+        categories_data = ['Aliments frais', 'Aliments emballés', 'Boissons', 'Produits de beauté et d\'hygiène', 'Nettoyage de la maison']
+        
+        for cat in categories_data:
+            Category.objects.get_or_create(label=cat)
 
-    for cat, prods in products_data.items():
-        category_instance = Category.objects.get(label=cat)
-        for prod in prods:
-            Product.objects.create(label=prod, description=f"Description pour {prod}", price=Money(10, 'EUR'), category=category_instance)
+        products_data = {
+            'Aliments frais': ['Poulet', 'Poisson', 'Jambon', 'Tomates', 'Laitue'],
+            'Aliments emballés': ['Pâtes', 'Riz', 'Céréales', 'Conserves de thon', 'Biscuits'],
+            'Boissons': ['Vin', 'Bière', 'Jus d\'orange', 'Eau', 'Limonade'],
+            'Produits de beauté et d\'hygiène': ['Shampooing', 'Gel douche', 'Crème hydratante', 'Dentifrice', 'Déodorant'],
+            'Nettoyage de la maison': ['Détergent', 'Nettoyant sol', 'Liquide vaisselle', 'Spray nettoyant', 'Eponge']
+        }
 
-    for cat in categories_data:
-        category_instance = Category.objects.get(label=cat)
-        products_in_category = Product.objects.filter(category=category_instance)[:2]
-        for prod in products_in_category:
-            Promotion.objects.create(start_date=timezone.now().date(), end_date=timezone.now().date() + timedelta(days=10), discount_percentage=10, product=prod)
+        default_image_path = 'muffin-g989da0f83_640_v9CY5WK.jpg'
 
-if __name__ == '__main__':
-    run()
+        for cat, prods in products_data.items():
+            category_instance = Category.objects.get(label=cat)
+            for prod in prods:
+                Product.objects.get_or_create(
+                    label=prod,
+                    defaults={
+                        'description': f"Description pour {prod}",
+                        'price': Money(10, 'EUR'),
+                        'category': category_instance,
+                        'image': f"{prod}.jpg"
+                    }
+                )
+
+        for cat in categories_data:
+            category_instance = Category.objects.get(label=cat)
+            products_in_category = Product.objects.filter(category=category_instance)[:2]
+            for prod in products_in_category:
+                Promotion.objects.get_or_create(
+                    product=prod,
+                    defaults={
+                        'start_date': timezone.now().date(),
+                        'end_date': timezone.now().date() + timedelta(days=10),
+                        'discount_percentage': 10
+                    }
+                )
