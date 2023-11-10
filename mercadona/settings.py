2b493dc8456de1f79774bd0effb0ad281ee3b159
Mohamed Bengrich <mohamed@etamin.studio>
2023-11-10 17:27:52 +0100

    change currency

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import moneyed
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mercadona.wsgi.application'
-DEFAULT_CURRENCY = moneyed.Currency('EUR')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
